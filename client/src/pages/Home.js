ea6ba814c074df8a2625861130681e11ea34bc7e
krasimirevlogiev <evlogievkrasi@gmail.com>
2024-10-16 21:46:37 +0300

    Frontend

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Home.css'; // Custom CSS for the home page
 
 function Home() {
   return (
-    <div>
+    <div className="home-container">
+      <div className="logo-section">
+        <img src="/logo-black.png" alt="Smoke Heaven" className="logo" />
+      </div>
       <h1>Welcome to Smoke Heaven</h1>
       <p>Browse our wide variety of smoking products.</p>
+      <div className="products-button">
+        <a href="/products" className="btn">Explore Products</a>
+      </div>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const mysql = require('mysql2');
+// const express = require('express');
+// const mysql = require('mysql2');
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-// MySQL connection
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'mern_user',
-  password: 'password',
-  database: 'mern_db'
-});
+// // MySQL connection
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'mern_user',
+//   password: 'password',
+//   database: 'mern_db'
+// });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
-});
+// db.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected to MySQL');
+// });
 
-app.get('/api/users', (req, res) => {
-  db.query('SELECT * FROM users', (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/api/users', (req, res) => {
+//   db.query('SELECT * FROM users', (err, results) => {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
 
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
